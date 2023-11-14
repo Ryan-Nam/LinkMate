055da6506684719b1af0078d33f4dca5b29cc71b
Ryan Nam <alfofh7060@gmail.com>
2023-11-15 00:38:58 +1100

    Fix deployment Issue

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function ContactCard({ contact }) {
-  const { name, username, email, address, phone, website, company } = contact;
+  // const { name, username, email, address, phone, website, company } = contact;
   const navigate = useNavigate();
 
   return (
     <li onClick={() => navigate(`contacts/${contact.id}`, { state: { contact } })}>
-    <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-      <img
-        src={`/images/people${contact.id}.webp`} 
-        alt={name}
-        className="w-12 h-12 rounded-full mr-3"
-      />
-      <span className='flex-1 ms-3 whitespace-nowrap'>{contact.name}</span>
-    </div>
-  </li>
+      <div className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <img
+          src={`/images/people${contact.id}.webp`} 
+          alt={contact.name}
+          className="w-12 h-12 rounded-full mr-3"
+        />
+        <span className='flex-1 ms-3 whitespace-nowrap'>{contact.name}</span>
+      </div>
+    </li>
   );
 }
