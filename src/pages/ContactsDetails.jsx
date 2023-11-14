c80baec43b4c61b205b9131f4b3844b622c19112
Ryan Nam <alfofh7060@gmail.com>
2023-11-15 00:28:01 +1100

    Add Deploy Issue

--- src/pages/ContactsDetails.jsx
+++ src/pages/ContactsDetails.jsx
@@ -19,7 +19,7 @@ export default function ContactsDetails() {
   return (
     <div className='p-4 sm:ml-64'>
       {/* Border */}
-      <div className='p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14'>
+      <div className='p-4 dark:border-gray-700 mt-14'>
         <div className='text-center mb-6'>
           <img
             src={`/images/people${contact.id}.webp`} 
